03283319071b06c24f28cb31f3c85b3132d3bf19
Michael Van Wesemael <michael@u2u.be>
2021-04-23 15:21:29 +0200

    added function

---
+++ api/TestFunction.cs
@@ -0,0 +1,24 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace U2U.API
+{
+    public static class TestFunction
+    {
+        [FunctionName("TestFunction")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+
+            return new OkObjectResult("Hello from API !!");
+        }
+    }
+}
